4da6f82cbb845377943ab4baa0b6a9c2485b7633
shd7abt <david.shugert@de.bosch.com>
2018-08-06 11:28:12 +0200

    Feat: Inquierer implementation

--- index.js
+++ index.js
@@ -1,23 +1,51 @@
-let lunchBreak
 const readline = require('readline')
+const inquirer = require('inquirer')
 let colors = require('colors')
-const Time = require('./Time.js')
 let program = require('commander')
+
+const Time = require('./Time.js')
 let busTimes = require('./busTimes')
 const defaultValues = require('./config.json')
-let arrivalT, requiredT, actualT
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-});
+
+let arrivalT, requiredT, actualT, lunchBreak, minimumTime
+
+const main = async () => {
+    program
+        .version(require('./package.json').version, '-v', '--version')
+        .description('Gitlab Command Line Tool for MBB')
+        .option('-t, --time <arrivalTime>', 'Defines the time of arrival of the worker')
+        .option('-l, --lunch [time]', 'Lunch pause, if left empty it will remove the lunch brake time')
+        .parse(process.argv)
+
+    lunchBreak = Time.stringToTime(program.lunch ? typeof program.lunch === 'string' ? program.lunch : null : defaultValues.breakTime)
+    initial(program.time ? program.time : (await inquirer.prompt(askArrival)).time)
+    departureTime = await inquirer.prompt(
+        {
+            type: 'input',
+            name: 'time',
+            message: 'Desired time to leave? \n'.bold,
+            default: minimumTime
+        }
+    )
+    calculate(departureTime.time)
+}
+
+
+
+const askArrival = {
+    type: 'input',
+    name: 'time',
+    message: 'Arrival Time? Format HR:Min accepted \n'.bold,
+    default: new Date(Date.now()).toLocaleTimeString()
+}
 
 const initial = (time) => {
     arrivalT = Time.joinTime(Time.stringToTime(time), lunchBreak)
     requiredT = Time.stringToTime(defaultValues.requiredTime)
     actualT = Time.stringToTime(new Date(Date.now()).toLocaleTimeString())
-
+    minimumTime = Time.joinTime(arrivalT, requiredT).toString()
     console.log(`\nArrival Time with Brake:${arrivalT}\n`)
-    console.log(`You must leave at ${Time.joinTime(arrivalT, requiredT).toString()}`.yellow.underline)
+    console.log(`You must leave at ${minimumTime}`.yellow.underline)
     console.log(`If you leave right now you will have: ${Time.joinTime(arrivalT, actualT, false).toString()}\n`.blue.underline)
 }
 
@@ -27,7 +55,7 @@ const calculate = (time) => {
     busTimes = busTimes.map(time => Time.stringToTime(time))
     matchingBusIndex = busTimes.map(time => Math.abs(Time.joinTime(desiredT, time, false).total)).reduce((iMin, x, i, arr) => x < arr[iMin] ? i : iMin, 0);
     console.log(`You should take the bus that leaves at ${busTimes[matchingBusIndex].toString()}`.blue.underline)
-    countdown(busTimes[matchingBusIndex])
+    //countdown(busTimes[matchingBusIndex])
     process.exit(0)
 }
 
@@ -35,7 +63,8 @@ countdown = (timeToLeave) => {
     actualT = Time.stringToTime(new Date(Date.now()).toLocaleTimeString())
     while (actualT.total < timeToLeave.total) {
         setTimeout(() => {
-            process.stdout.write("\r\x1b[K")
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
             console.log(`Time left: ${joinTime(actualT, timeToLeave, false).toString}`)
         }, 1000)
         actualT = Time.stringToTime(new Date(Date.now()).toLocaleTimeString())
@@ -43,26 +72,9 @@ countdown = (timeToLeave) => {
     }
     console.log('finish')
 }
-program
-    .version(require('./package.json').version, '-v', '--version')
-    .description('Gitlab Command Line Tool for MBB')
-    .option('-t, --time <arrivalTime>', 'Defines the time of arrival of the worker')
-    .option('-l, --lunch [time]', 'Lunch pause, if left empty it will remove the lunch brake time')
-    .parse(process.argv)
 
-lunchBreak = Time.stringToTime(program.lunch ? typeof program.lunch === 'string' ? program.lunch : null : defaultValues.breakTime)
+main().catch(err => console.error(err))
 
-if (program.time) {
-    initial(program.time)
-    rl.question('Desired time to leave? \n'.bold, (answer) => {
-        calculate(answer)
-    })
-} else {
-    rl.question('Arrival Time? Format HR:Min accepted \n'.bold, (answer) => {
-        initial(answer)
-        process.exit(0)
-    })
-}
 
 
 
---
+++ test.js
@@ -0,0 +1,4 @@
+console.log('Hello')
+process.stdout.clearLine();
+process.stdout.cursorTo(0);
+console.log('HEy')
