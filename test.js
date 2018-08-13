361d6d722c0af05f4485a0a61f9c50aa6042b726
shd7abt <david.shugert@de.bosch.com>
2018-08-13 09:21:45 +0200

    Finisged countdown

--- test.js
+++ test.js
@@ -1,10 +1,2 @@
-let word = 'troport'
-consolccisPalindrome(word)
-isPalindrome = (word) => {
-    flag = false
-    for (let [i, v] of word) {
-        if (word[word.length - i] === v) {
-            flag = true
-        }
-    }
-}
+HTMLAreasCollection
+pallap
